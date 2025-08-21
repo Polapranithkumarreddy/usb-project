885a8c89f4631257df3bb5efe801395beb95b55e
Polapranithkumarreddy <polapranith567@gmail.com>
2025-08-21 12:04:07 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,14 @@ export default function RootLayout({ children }) {
       >
         <Navbar />
         {children}
+        <footer className=" bg-gray-200 py-6 text-center text-gray-600">
+  <p>
+    &copy; {new Date().getFullYear()} Supraja Technologies. All rights reserved.
+  </p>
+</footer>
       </body>
+      
+
     </html>
   );
 }
